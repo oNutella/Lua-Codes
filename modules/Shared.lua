1687d70831c74c809c69587a2f9d4bc3ef38b962
Nutella <miguelsimwr@gmail.com>
2026-05-21 14:10:05 -0300

    Update Shared.lua

--- modules/Shared.lua
+++ modules/Shared.lua
@@ -1,33 +1,58 @@
--- modules/Shared.lua
-
 return {
-    COLOR_ON     = Color3.fromRGB(0,   140, 140),
+    COLOR_ON     = Color3.fromRGB(61, 143, 220),
     COLOR_OFF    = Color3.fromRGB(100, 100, 100),
-    COLOR_YELLOW = Color3.fromRGB(180, 140, 0),
+    COLOR_YELLOW = Color3.fromRGB(255, 170, 0),
 
-    SF_EXPANDED  = UDim2.new(0, 183, 0, 400),
-    SF_COLLAPSED = UDim2.new(0, 17,  0, 400),
-    SF_TWEEN     = TweenInfo.new(0.4, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
+    SF_EXPANDED  = UDim2.new(0, 170, 0, 400),
+    SF_COLLAPSED = UDim2.new(0, 17, 0, 400),
+
+    SF_TWEEN = TweenInfo.new(
+        0.25,
+        Enum.EasingStyle.Quad,
+        Enum.EasingDirection.Out
+    ),
 
     TRACER_COLORS = {
-        Color3.fromRGB(100, 100, 100),
-        Color3.fromRGB(112, 0,   112),
-        Color3.fromRGB(255, 0,   0),
-        Color3.fromRGB(0,   0,   255),
-        Color3.fromRGB(0,   200, 0),
-        Color3.fromRGB(0,   0,   0),
+        Color3.fromRGB(255, 0, 0),
+        Color3.fromRGB(0, 255, 0),
+        Color3.fromRGB(0, 170, 255),
         Color3.fromRGB(255, 255, 255),
-    },
-
-    APPLIER_STATES = {
-        all    = { color = Color3.fromRGB(100, 100, 100), text = "Apply to All"    },
-        me     = { color = Color3.fromRGB(180, 140, 0),   text = "Apply to Me"     },
-        others = { color = Color3.fromRGB(0,   140, 140), text = "Apply to Others" },
+        Color3.fromRGB(0, 0, 0),
     },
 
     TRACER_STATES = {
-        none   = { color = Color3.fromRGB(100, 100, 100), text = "Gun Tracers: ON"        },
-        others = { color = Color3.fromRGB(180, 140, 0),   text = "Other Gun Tracers: OFF" },
-        all    = { color = Color3.fromRGB(0,   140, 140), text = "All Gun Tracers: OFF"   },
+        none = {
+            color = Color3.fromRGB(100, 100, 100),
+            text  = "Gun Tracers: OFF",
+        },
+
+        others = {
+            color = Color3.fromRGB(255, 170, 0),
+            text  = "Gun Tracers: Others",
+        },
+
+        all = {
+            color = Color3.fromRGB(61, 143, 220),
+            text  = "Gun Tracers: ALL",
+        },
     },
+
+    APPLIER_STATES = {
+        all = {
+            color = Color3.fromRGB(100, 100, 100),
+            text  = "Apply to All",
+        },
+
+        me = {
+            color = Color3.fromRGB(255, 170, 0),
+            text  = "Apply to Me",
+        },
+
+        others = {
+            color = Color3.fromRGB(61, 143, 220),
+            text  = "Apply to Others",
+        },
+    },
+
+    NamecallHooks = {},
 }
